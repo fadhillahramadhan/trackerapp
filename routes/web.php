b223172fd30a14e94fbe8c6b1df743b957cbdfe6
fadhillahramadhan <fadhillahramadhan01@gmail.com>
2023-12-31 12:58:27 +0700

    add;web

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,36 @@ Route::get('/', [TrackerController::class, 'show']);
 // Route::get()
 // show echo shwo
 Route::get('/show', function () {
-    echo "show";
+    // echo "show";
+    // get location from cloudflare
+    $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+    echo $ip;
+    // get location from ipinfo.io
+    $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+    echo $details->city; // -> is used to access the object property
+    echo $details->country;
+    echo $details->region;
+    echo $details->loc;
+    echo $details->postal;
+    echo $details->timezone;
+    echo $details->readme;
+    // get location from ip-api.com
+    $details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
+    echo $details->city; // -> is used to access the object property
+    echo $details->country;
+    echo $details->region;
+    echo $details->loc;
+    echo $details->postal;
+
+    // get location from ip-api.com
+    $details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
+    echo $details->city; // -> is used to access the object property
+    echo $details->country;
+    echo $details->region;
+    echo $details->loc;
+    echo $details->postal;
+    echo $details->timezone;
+    echo $details->readme;
+    // get location from ip-api.com
+
 });
