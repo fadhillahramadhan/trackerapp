a384212f0f07c7de834eb7b0bd05980102b5d197
fadhillahramadhan <fadhillahramadhan01@gmail.com>
2023-12-31 13:01:51 +0700

    add: lat long

--- routes/web.php
+++ routes/web.php
@@ -20,33 +20,32 @@ Route::get('/show', function () {
     // echo "show";
     // get location from cloudflare
     $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
-    echo $ip;
+    echo $ip . "<br>";
     // get location from ipinfo.io
     $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
-    echo $details->city; // -> is used to access the object property
-    echo $details->country;
-    echo $details->region;
-    echo $details->loc;
-    echo $details->postal;
-    echo $details->timezone;
-    echo $details->readme;
-    // get location from ip-api.com
-    $details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
-    echo $details->city; // -> is used to access the object property
-    echo $details->country;
-    echo $details->region;
-    echo $details->loc;
-    echo $details->postal;
+    echo $details->city . "<br>"; // -> is used to access the object property
+    echo $details->country . "<br>";
+    echo $details->region . "<br>";
+    echo $details->loc . "<br>";
+    echo $details->postal . "<br>";
 
-    // get location from ip-api.com
-    $details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
-    echo $details->city; // -> is used to access the object property
-    echo $details->country;
-    echo $details->region;
-    echo $details->loc;
-    echo $details->postal;
-    echo $details->timezone;
-    echo $details->readme;
+
+    // get location from google maps
+    $url = "http://maps.googleapis.com/maps/api/geocode/json?latlng={$details->loc}&sensor=false";
+    $data = @file_get_contents($url);
+    $jsondata = json_decode($data, true);
+    $city = $jsondata["results"][0]["address_components"][2]["long_name"];
+    $country = $jsondata["results"][0]["address_components"][5]["long_name"];
+    // coordinates
+    $lat = $jsondata["results"][0]["geometry"]["location"]["lat"];
+    $lng = $jsondata["results"][0]["geometry"]["location"]["lng"];
+
+    echo $lat . "<br>";
+    echo $city . "<br>";
+    echo $country . "<br>";
+    echo $url . "<br>";
+
+
     // get location from ip-api.com
 
 });
